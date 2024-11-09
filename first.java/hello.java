bc09e2c9a18046b80ecd52d8a2f395284d53e817
Karthikeya-2208 <nagillakarthikeya2004@gmail.com>
2024-11-09 16:30:20 +0530

    Update hello.java

--- first.java/hello.java
+++ first.java/hello.java
@@ -3,7 +3,7 @@ package first.java;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello github"); 
-                System.out.println("hello github"); 
+        System.out.println("hello github"); 
 
     }
 }
